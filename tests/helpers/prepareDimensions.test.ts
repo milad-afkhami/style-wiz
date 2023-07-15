b4b976cedb72f1c54420f1e294c4fea2bba4121f
Milad Afkhami <dev.miladafkhami@gmail.com>
2023-07-15 17:36:10 +0330

    test(helpers): adding test to the `prepareDimensions` helper

---
+++ tests/helpers/prepareDimensions.test.ts
@@ -0,0 +1,24 @@
+import prepareDimensions from "@helpers/prepareDimensions";
+
+const givenParam = {
+  top: "var(--css-variable)",
+  right: "2px",
+  bottom: "3rem",
+  left: 4,
+} as Parameters<typeof prepareDimensions>[0];
+
+const expectedResult = {
+  top: "var(--css-variable)",
+  right: "var(--spacing-2px, 2px)",
+  bottom: "var(--spacing-3rem, 3rem)",
+  left: "var(--spacing-4, 4)",
+};
+
+it("maps correct values for each dimension", () => {
+  const mockFn = jest.fn(prepareDimensions);
+  const value = mockFn(givenParam);
+
+  expect(mockFn).toBeCalledTimes(1);
+  expect(mockFn).toHaveBeenCalledWith(givenParam);
+  expect(value).toEqual(expectedResult);
+});
